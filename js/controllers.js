0f04034a7042c30b01e0eb37350f0a416ef01d4d
mhariznaim <mhariznaim@gmail.com>
2018-01-23 19:59:17 +0800

    changes to the search page with button for navigation

--- js/controllers.js
+++ js/controllers.js
@@ -1,14 +1,19 @@
-angular.module('app.controllers', [])
+angular.module('app.controllers', ['ionic', 'ngCordova'])
   
-.controller('muetDictionaryCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('muetDictionaryCtrl',  // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
- $scope.fnWohaa = function(txtSearch){
-	 alert("you pressed:"+txtSearch);
+function ($scope, $stateParams,$state,$ionicPlatform) {
+ $scope.searching = function(searchString){
+	 //alert("you pressed :"+searchString);
+	
+ };
+ 
+ $scope.fnSearch = function(){
+	$state.go('menu.wordMeaning');
  };
 
-}])
+})
    
 .controller('wordMeaningCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
