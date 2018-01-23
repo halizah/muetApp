a43973930ba09dc744f9611459ef13c75c413164
mhariznaim <mhariznaim@gmail.com>
2018-01-23 20:57:53 +0800

    mock autocomplete for searchbox

--- js/controllers.js
+++ js/controllers.js
@@ -1,13 +1,22 @@
-angular.module('app.controllers', ['ionic', 'ngCordova'])
+angular.module('app.controllers', ['ionic', 'ngCordova','autoCompleteModule'])
   
 .controller('muetDictionaryCtrl',  // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams,$state,$ionicPlatform) {
- $scope.searching = function(searchString){
-	 //alert("you pressed :"+searchString);
-	
- };
+$scope.searchString = null;
+$scope.autoCompleteOptions = {
+            minimumChars: 1,
+            data: function (searchText) {
+                searchText = searchText.toUpperCase();
+
+                var colors = _.filter(MOCK_CSS_COLORS, function (color) {
+                    return color.name.startsWith(searchText);
+                });
+
+                return _.pluck(colors, 'name');
+            }
+        }
  
  $scope.fnSearch = function(){
 	$state.go('menu.wordMeaning');
